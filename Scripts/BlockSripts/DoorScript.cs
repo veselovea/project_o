b277def37f2e5e35d99bb43f1aba60d04c688c27
Данил Никитин <DN-2003@yandex.ru>
2023-04-10 18:12:45 +0800

    Refactoring again(2x)

--- Scripts/BlockSripts/DoorScript.cs
+++ Scripts/BlockSripts/DoorScript.cs
@@ -5,25 +5,40 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DoorScript : MonoBehaviour
+public class DoorScript : ResourceBlock
 {
-    public ResourceBlock resourceBlock = new ResourceBlock();
+    public override int Durability { get; set; }
+    public override ResourcesFromBlocks Type { get; set; }
 
     private void Start()
     {
-        resourceBlock.Durability = 50;
-        resourceBlock.Type = ResourcesFromBlocks.Door;
+        Durability = 50;
+        Type = ResourcesFromBlocks.Door;
     }
 
     private void Update()
     {
-        if (transform.Find("ButtonText").gameObject.activeSelf)
+        if (transform.Find("ButtonText").gameObject.activeSelf && Input.GetKeyDown(KeyCode.E))
         {
-            transform.Find("ButtonText").gameObject.SetActive(false);
+            //закрыть
+            
+        }
+        else if(!transform.Find("ButtonText").gameObject.activeSelf && Input.GetKeyDown(KeyCode.E))
+        {
+            //открыть
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            transform.Find("ButtonText").gameObject.SetActive(true);
         }
         else
         {
-            transform.Find("ButtonText").gameObject.SetActive(true);
+            transform.Find("ButtonText").gameObject.SetActive(false);
         }
     }
 }
